ab7edc8b51e243ed54377018cb64e0b8652c2434
Mubashar Javed <mubasharbravo302@gmail.com>
2022-11-10 01:39:35 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,124 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect, useState } from "react";
+import Map, {
+  ControlPosition,
+  FullscreenControl,
+  GeolocateControl,
+  Marker,
+  NavigationControl,
+  Popup,
+  ScaleControl
+} from "react-map-gl";
+import "./App.css";
+import { ControlDropDown, ExampleResponse, Feature } from "./app.model";
+
+const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+function App() {
+  const [geoData, setGeoData] = useState<ExampleResponse>(
+    {} as ExampleResponse
+  );
+  const [showControls, setShowControls] = useState(true);
+  const [position, setPosition] = useState<ControlPosition>("bottom-right");
+  const [popupInfo, setPopupInfo] = useState<Feature | null>(null);
+
+  const controlPositions: ControlDropDown[] = [
+    { value: "top-left", label: "Top Left" },
+    { value: "top-right", label: "Top Right" },
+    { value: "bottom-left", label: "Bottom Left" },
+    { value: "bottom-right", label: "Bottom Right" },
+  ];
+
+  const toggleControls = () => {
+    setShowControls(!showControls);
+  };
+
+  const updatePosition = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const position = e.target.value as ControlPosition;
+    console.log("position is", position);
+    if (position) {
+      setPosition("bottom-right");
+      // FIXME: this is not working debug later.
+    }
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3001/example")
+      .then((response) => response.json())
+      .then((data) => {
+        setGeoData(data);
+      });
+  }, []);
+
+  return (
+    <div className="container mt-3">
+      <Map
+        initialViewState={{
+          latitude: 31,
+          longitude: 74,
+          zoom: 0,
+        }}
+        style={{ width: "100%", height: "50vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxAccessToken={ACCESS_TOKEN}
+      >
+        {showControls && (
+          <div>
+            <ScaleControl maxWidth={100} unit="metric" position={position} />
+            <NavigationControl
+              showCompass={true}
+              showZoom={true}
+              position={position}
+            />
+            <FullscreenControl position={position} />
+            <GeolocateControl position={position} />
+          </div>
+        )}
+        {geoData.features &&
+          geoData.features.map((feature, index) => (
+            <Marker
+              key={index}
+              latitude={feature.properties.Latitude}
+              longitude={feature.properties.Longitude}
+              onClick={(e) => {
+                e.originalEvent.stopPropagation();
+                setPopupInfo(feature);
+              }}
+            >
+              <i className="bi bi-geo-alt-fill h4"></i>
+            </Marker>
+          ))}
+        {popupInfo && (
+          <Popup
+            latitude={popupInfo.properties.Latitude}
+            longitude={popupInfo.properties.Longitude}
+            anchor="bottom"
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setPopupInfo(null)}
+          >
+            <div
+              className="mt-3"
+              style={{ minHeight: "1.875rem", maxHeight: "2.5rem" }}
+            >
+              {popupInfo.properties.ObjectType} --- {popupInfo.properties.City}
+            </div>
+          </Popup>
+        )}
+      </Map>
+      <button className="btn btn-primary" onClick={toggleControls}>
+        {showControls ? "Hide" : "Show"} Controls
+      </button>
+      <select className="form-control" onChange={updatePosition}>
+        <option value="">--- Select Control positions ---</option>
+        {controlPositions.map((position, index) => (
+          <option value={position.value} key={index}>
+            {position.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default App;
